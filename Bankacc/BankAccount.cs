95865fe25eb0e757402992d0098c88d7eb41c580
pramataroff <milenpramatarov@gmail.com>
2024-04-21 23:53:31 +0300

    milen

---
+++ Bankacc/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankacc
+{
+    public class BankAccount
+    {
+        public int id;
+        public double balance;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public void Deposit(double amount)
+        {
+            this.balance += amount;
+        }
+        public void WithDraw(double amount)
+        {
+            this.balance -= amount;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Account {this.Id}, Balance {this.Balance}.");
+        }
+    }
+}
---
+++ Bankacc/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bankacc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.Id = 1;
+
+            account.Deposit(15);
+            account.WithDraw(8);
+            account.Print();
+        }
+    }
+}
